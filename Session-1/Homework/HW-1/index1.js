fcbf13ef1d6a95c2730c09c1392b5d5d72c67f48
Tú Mì <kfcvswc@gmail.com>
2020-11-16 21:36:31 +0700

    add full homework

---
+++ Session-1/Homework/HW-1/index1.js
@@ -0,0 +1,8 @@
+console.log (`1. To check a variable's type, use typeof operator.`);
+console.log (`Eg: \n`);
+var str = "It's only words, and words are all I have";
+typeof str; // returns string for sure
+console.log('2. In JS, SyntaxError will be gotten from the compiler telling you that some of your variables have invalid names, and they are: ');
+console.log('- The first letter start with a number');
+console.log('- Contains special character(s) except $ and _ ');
+console.log('- Can not be a keyword such as let, etc...');
